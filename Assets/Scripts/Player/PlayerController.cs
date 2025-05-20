b114eb47e7cb2310cbf8d6598caf52f662ee565e
wowyesol <bmo11152598@gmail.com>
2025-05-20 13:24:24 +0900

    0520_Arrow

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Tilemaps;
+using UnityEngine.UI;
 
 public enum MoveDir
 {
@@ -19,18 +20,32 @@ public class PlayerController : MonoBehaviour
     public float speed = 5f;
     public Animator animator;
     public SpriteRenderer spriteRenderer;
+    private Tilemap cabinetTilemap;
 
     private Rigidbody2D rb;
     private Vector3Int cellPos = Vector3Int.zero;
     private Vector2 lastMoveInput = Vector2.down;
     private bool isMoving = false;
     private MoveDir dir = MoveDir.None;
+    
+    public GameObject arrowPrefab;
+    public float arrowSpeed = 10f;
+    public float arrowLife =2f;
+
+    public Image arrowLoading;
+    private float timer = 0;
+    private bool isCooldown = false;
+    private float arrowCooldown = 1.5f;
+    private float lastArrowTime = -999f;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         if (grid == null)
             grid = MapManager.Instance.grid;
+        
+        GameObject cabinetObj = GameObject.Find("Cabinet");
+        cabinetTilemap = cabinetObj.GetComponent<Tilemap>();
     }
 
     private void Start()
@@ -44,7 +59,43 @@ public class PlayerController : MonoBehaviour
         DirInput();
         UpdateMoving();
         UpdateAnimator();
+        
+        if (Keyboard.current.rightShiftKey.wasPressedThisFrame && Time.time - lastArrowTime >= arrowCooldown)
+        {
+            ShootArrow();
+            lastArrowTime = Time.time;
+            isCooldown = true;
+            
+            arrowLoading.fillAmount = 0;
+        }
+
+        if (isCooldown)
+        {
+            timer += Time.deltaTime;
+            float ratio = Mathf.Clamp01(timer / arrowCooldown);
+            arrowLoading.fillAmount = ratio;
+
+            if (timer >= arrowCooldown)
+            {
+                isCooldown = false;
+                timer = 0;
+                arrowLoading.fillAmount = 1;
+            }
+        }
+        
+        Vector3Int currentCell = grid.WorldToCell(transform.position);
+        Vector3Int currentTop = currentCell + Vector3Int.up;
+        
+        if (cabinetTilemap.HasTile(currentCell) && cabinetTilemap.HasTile(currentTop))
+        {
+            SetPlayerAlpha(0.6f);
+        }
+        else
+        {
+            SetPlayerAlpha(1f);
+        }
     }
+    
 
     private void FixedUpdate()
     {
@@ -134,5 +185,48 @@ public class PlayerController : MonoBehaviour
         animator.SetFloat("XInput", moveInput.x);
         animator.SetFloat("YInput", moveInput.y);
         animator.SetBool("isWalk", isMoving && dir != MoveDir.None);
+    }
+    
+    void ShootArrow()
+    {
+        Vector2 shootDir = lastMoveInput.normalized;
+
+        Vector2 spawnOffset = Vector2.zero;
+        if (shootDir == Vector2.up)
+            spawnOffset = new Vector2(0f, 1f);
+        else if (shootDir == Vector2.down)
+            spawnOffset = new Vector2(0f, 0f); 
+        else if (shootDir == Vector2.left)
+            spawnOffset = new Vector2(-0.5f, 0.7f);
+        else if (shootDir == Vector2.right)
+            spawnOffset = new Vector2(0.5f, 0.7f);
+        
+        Vector2 spawnPos = rb.position + spawnOffset;
+        
+        GameObject arrow = Instantiate(arrowPrefab, spawnPos, Quaternion.identity);
+
+        Rigidbody2D arrowRb = arrow.GetComponent<Rigidbody2D>();
+        arrowRb.linearVelocity = shootDir * arrowSpeed;
+        
+        float zRot = 0f;
+        if (shootDir == Vector2.up)
+            zRot = 180f;
+        else if (shootDir == Vector2.down)
+            zRot = 0f;
+        else if (shootDir == Vector2.left)
+            zRot = -90f;
+        else if (shootDir == Vector2.right)
+            zRot = 90f;
+
+        arrow.transform.rotation = Quaternion.Euler(0f, 0f, zRot);
+
+        Destroy(arrow, arrowLife);
+    }
+    
+    void SetPlayerAlpha(float alpha)
+    {
+        Color c = spriteRenderer.color;
+        c.a = alpha;
+        spriteRenderer.color = c;
     }
 }
