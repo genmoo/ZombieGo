0f810760c9168e6b153942338dc204daf403da5e
wowyesol <bmo11152598@gmail.com>
2025-05-21 18:08:14 +0900

    0521_Zombiemap,dash

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -33,25 +33,36 @@ public class PlayerController : MonoBehaviour
     private Vector2 lastMoveInput = Vector2.down;
     private bool isMoving = false;
     private MoveDir dir = MoveDir.None;
-    
+
     public GameObject arrowPrefab;
     public float arrowSpeed = 10f;
-    public float arrowLife =2f;
+    public float arrowLife = 2f;
 
     public Image arrowLoading;
+    public Image arrowImage;
     private float timer = 0;
     private bool isCooldown = false;
     private float arrowCooldown = 1.5f;
     private float lastArrowTime = -999f;
-    
+
+    public Image dashLoading;
+    public Image dashImage;
+    private float timer2 = 0;
+    private bool isCooldown2 = false;
+    private float dashCooldown = 8f;
+    private float lastDashTime = -999f;
+
     public PlayerState playerState = PlayerState.Human;
+    public RuntimeAnimatorController zombieAnimator;
+    private bool zombieSetupDone = false;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         if (grid == null)
             grid = MapManager.Instance.grid;
-        
+
+
         GameObject cabinetObj = GameObject.Find("Cabinet");
         cabinetTilemap = cabinetObj.GetComponent<Tilemap>();
     }
@@ -59,7 +70,12 @@ public class PlayerController : MonoBehaviour
     private void Start()
     {
         cellPos = grid.WorldToCell(transform.position);
-        rb.position = grid.CellToWorld(cellPos) + new Vector3(0.5f, 0f); 
+        rb.position = grid.CellToWorld(cellPos) + new Vector3(0.5f, 0f);
+
+        if (playerState == PlayerState.Zombie)
+        {
+            BecomeZombie();
+        }
     }
 
     private void Update()
@@ -72,47 +88,14 @@ public class PlayerController : MonoBehaviour
         {
             ArrowInput();
         }
+
+        else if (playerState == PlayerState.Zombie && !zombieSetupDone)
+        {
+            BecomeZombie();
+        }
+
+        CabinetAlpha();
         
-        else if (playerState == PlayerState.Zombie)
-        {
-            
-        }
-    }
-
-    void ArrowInput()
-    {
-        if (Keyboard.current.rightShiftKey.wasPressedThisFrame && Time.time - lastArrowTime >= arrowCooldown)
-        {
-            ShootArrow();
-            lastArrowTime = Time.time;
-            isCooldown = true;
-
-            arrowLoading.fillAmount = 0;
-        }
-
-        if (isCooldown)
-        {
-            timer += Time.deltaTime;
-            float ratio = Mathf.Clamp01(timer / arrowCooldown);
-            arrowLoading.fillAmount = ratio;
-
-            if (timer >= arrowCooldown)
-            {
-                isCooldown = false;
-                timer = 0;
-                arrowLoading.fillAmount = 1;
-            }
-        }
-
-        Vector3Int currentCell = grid.WorldToCell(transform.position);
-        if (cabinetTilemap.HasTile(currentCell))
-        {
-            SetPlayerAlpha(0.6f);
-        }
-        else
-        {
-            SetPlayerAlpha(1f);
-        }
     }
 
     private void FixedUpdate()
@@ -144,12 +127,12 @@ public class PlayerController : MonoBehaviour
         Vector3Int nextCell = cellPos;
         switch (dir)
         {
-            case MoveDir.Up:    nextCell += Vector3Int.up; break;
-            case MoveDir.Down:  nextCell += Vector3Int.down; break;
-            case MoveDir.Left:  nextCell += Vector3Int.left; break;
+            case MoveDir.Up: nextCell += Vector3Int.up; break;
+            case MoveDir.Down: nextCell += Vector3Int.down; break;
+            case MoveDir.Left: nextCell += Vector3Int.left; break;
             case MoveDir.Right: nextCell += Vector3Int.right; break;
         }
-        
+
         Vector3Int topCell = nextCell + Vector3Int.up;
 
         if (!wallTilemap.HasTile(nextCell) && !wallTilemap.HasTile(topCell))
@@ -195,16 +178,19 @@ public class PlayerController : MonoBehaviour
         if (dir != MoveDir.None)
             lastMoveInput = moveInput;
 
-        if (lastMoveInput.x > 0)
+        if (dir == MoveDir.Right)
             spriteRenderer.flipX = true;
-        else if (lastMoveInput.x < 0)
+        else if (dir == MoveDir.Left)
+            spriteRenderer.flipX = false;
+
+        if (dir == MoveDir.Up || dir == MoveDir.Down)
             spriteRenderer.flipX = false;
 
         animator.SetFloat("XInput", moveInput.x);
         animator.SetFloat("YInput", moveInput.y);
         animator.SetBool("isWalk", isMoving && dir != MoveDir.None);
     }
-    
+
     void ShootArrow()
     {
         Vector2 shootDir = lastMoveInput.normalized;
@@ -213,19 +199,19 @@ public class PlayerController : MonoBehaviour
         if (shootDir == Vector2.up)
             spawnOffset = new Vector2(0f, 1f);
         else if (shootDir == Vector2.down)
-            spawnOffset = new Vector2(0f, 0f); 
+            spawnOffset = new Vector2(0f, 0f);
         else if (shootDir == Vector2.left)
             spawnOffset = new Vector2(-0.5f, 0.7f);
         else if (shootDir == Vector2.right)
             spawnOffset = new Vector2(0.5f, 0.7f);
-        
+
         Vector2 spawnPos = rb.position + spawnOffset;
-        
+
         GameObject arrow = Instantiate(arrowPrefab, spawnPos, Quaternion.identity);
 
         Rigidbody2D arrowRb = arrow.GetComponent<Rigidbody2D>();
         arrowRb.linearVelocity = shootDir * arrowSpeed;
-        
+
         float zRot = 0f;
         if (shootDir == Vector2.up)
             zRot = 180f;
@@ -240,11 +226,68 @@ public class PlayerController : MonoBehaviour
 
         Destroy(arrow, arrowLife);
     }
-    
+
+    void DashAttack()
+    {
+
+    }
+
     void SetPlayerAlpha(float alpha)
     {
         Color c = spriteRenderer.color;
         c.a = alpha;
         spriteRenderer.color = c;
+    }
+
+    void ArrowInput()
+    {
+        if (Keyboard.current.rightShiftKey.wasPressedThisFrame && Time.time - lastArrowTime >= arrowCooldown)
+        {
+            ShootArrow();
+            lastArrowTime = Time.time;
+            isCooldown = true;
+
+            arrowLoading.fillAmount = 0;
+        }
+
+        if (isCooldown)
+        {
+            timer += Time.deltaTime;
+            float ratio = Mathf.Clamp01(timer / arrowCooldown);
+            arrowLoading.fillAmount = ratio;
+
+            if (timer >= arrowCooldown)
+            {
+                isCooldown = false;
+                timer = 0;
+                arrowLoading.fillAmount = 1;
+            }
+        }
+    }
+
+    void BecomeZombie()
+    {
+        zombieSetupDone = true;
+
+        arrowLoading.gameObject.SetActive(false);
+        arrowImage.gameObject.SetActive(false);
+
+        dashLoading.gameObject.SetActive(true);
+        dashImage.gameObject.SetActive(true);
+
+        animator.runtimeAnimatorController = zombieAnimator;
+    }
+
+    void CabinetAlpha()
+    {
+        Vector3Int currentCell = grid.WorldToCell(transform.position);
+        if (cabinetTilemap.HasTile(currentCell))
+        {
+            SetPlayerAlpha(0.5f);
+        }
+        else
+        {
+            SetPlayerAlpha(1f);
+        }
     }
 }
