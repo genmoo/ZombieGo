d96c1972280eb9cb38a8542607b6bedd5ce8ff41
wowyesol <bmo11152598@gmail.com>
2025-05-22 18:30:58 +0900

    0522_Dash

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -45,12 +45,22 @@ public class PlayerController : MonoBehaviour
     private float arrowCooldown = 1.5f;
     private float lastArrowTime = -999f;
 
+    
     public Image dashLoading;
     public Image dashImage;
     private float timer2 = 0;
     private bool isCooldown2 = false;
-    private float dashCooldown = 8f;
+    public float dashCooldown = 8f;
     private float lastDashTime = -999f;
+    
+    private bool isDashing = false;
+    private Vector2 dashTarget;
+    private float dashSpeed = 30f;
+    private Vector2 dashDirection = Vector2.zero;
+    private float dashLock = 0;
+    
+    private float ghostSpawnTimer = 0f;
+    private float ghostSpawnInterval = 0.01f;
 
     public PlayerState playerState = PlayerState.Human;
     public RuntimeAnimatorController zombieAnimator;
@@ -87,6 +97,10 @@ public class PlayerController : MonoBehaviour
         if (playerState == PlayerState.Human)
         {
             ArrowInput();
+        } 
+        else if (playerState == PlayerState.Zombie)
+        {
+            DashInput();
         }
 
         else if (playerState == PlayerState.Zombie && !zombieSetupDone)
@@ -100,11 +114,49 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if (isDashing)
+        {
+            Vector2 nextPos = Vector2.MoveTowards(rb.position, dashTarget, dashSpeed * Time.fixedDeltaTime);
+            Vector3Int nextCell = grid.WorldToCell(nextPos);
+            
+            if (wallTilemap.HasTile(nextCell))
+            {
+                isDashing = false;
+                dashLock = Time.time + 0.5f;
+                cellPos = grid.WorldToCell(rb.position);
+                return;
+            }
+
+            rb.position = nextPos;
+            
+            ghostSpawnTimer += Time.fixedDeltaTime;
+            if (ghostSpawnTimer >= ghostSpawnInterval)
+            {
+                CreateDashGhost();
+                ghostSpawnTimer = 0f;
+            }
+            
+            if (Vector2.Distance(rb.position, dashTarget) < 0.05f)
+            {
+                rb.position = dashTarget;
+                isDashing = false;
+                cellPos = grid.WorldToCell(rb.position);
+                dashLock = Time.time + 0.5f;
+            }
+            return; // 대시 중엔 일반 이동 금지
+        }
+        
         UpdatePosition();
     }
 
     void DirInput()
     {
+        if (Time.time < dashLock)
+        {
+            dir = MoveDir.None;
+            return;
+        }
+        
         var keyboard = Keyboard.current;
 
         if (keyboard.wKey.isPressed)
@@ -135,7 +187,7 @@ public class PlayerController : MonoBehaviour
 
         Vector3Int topCell = nextCell + Vector3Int.up;
 
-        if (!wallTilemap.HasTile(nextCell) && !wallTilemap.HasTile(topCell))
+        if (!wallTilemap.HasTile(nextCell))
         {
             cellPos = nextCell;
             isMoving = true;
@@ -227,9 +279,68 @@ public class PlayerController : MonoBehaviour
         Destroy(arrow, arrowLife);
     }
 
-    void DashAttack()
+    void DashInput()
     {
+        if (isDashing) return;
+        
+        if (Keyboard.current.rightShiftKey.wasPressedThisFrame &&
+            Time.time - lastDashTime > dashCooldown)
+        {
+            if (lastMoveInput == Vector2.up) dashDirection = Vector2Int.up;
+            else if (lastMoveInput == Vector2.down) dashDirection = Vector2Int.down;
+            else if (lastMoveInput == Vector2.left) dashDirection = Vector2Int.left;
+            else if (lastMoveInput == Vector2.right) dashDirection = Vector2Int.right;
+            else return;
+            
+            Vector3Int startCell = grid.WorldToCell(rb.position);
+            Vector3Int dashCell = startCell;
 
+            for (int i = 0; i < 6; i++)
+            {
+                Vector3Int nextCell = dashCell + new Vector3Int((int)dashDirection.x , (int)dashDirection.y, 0);
+                if (wallTilemap.HasTile(nextCell))
+                    break;
+                dashCell = nextCell;
+            }
+
+            dashTarget = grid.CellToWorld(dashCell) + new Vector3(0.5f, 0f);
+            isDashing = true;
+            
+            lastDashTime = Time.time;
+            isCooldown2 = true;
+            dashLoading.fillAmount = 0;
+        }
+        
+        if (isCooldown2)
+        {
+            timer2 += Time.deltaTime;
+            float ratio = Mathf.Clamp01(timer2 / dashCooldown);
+            dashLoading.fillAmount = ratio;
+
+            if (timer2 >= dashCooldown)
+            {
+                isCooldown2 = false;
+                timer2 = 0;
+                dashLoading.fillAmount = 1;
+            }
+        }
+    }
+    
+    void CreateDashGhost()
+    {
+        GameObject ghost = new GameObject("DashGhost");
+        SpriteRenderer sr = ghost.AddComponent<SpriteRenderer>();
+
+        sr.sprite = spriteRenderer.sprite;
+        sr.flipX = spriteRenderer.flipX;
+        sr.sortingOrder = spriteRenderer.sortingOrder - 1; 
+
+        sr.color = new Color(1f, 1f, 1f, 0.5f); 
+
+        ghost.transform.position = transform.position;
+        ghost.transform.localScale = transform.localScale;
+
+        Destroy(ghost, 0.2f);
     }
 
     void SetPlayerAlpha(float alpha)
