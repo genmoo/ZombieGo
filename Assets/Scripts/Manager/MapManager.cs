171f885a8454b6949929fa3a296f8882d45a1d41
larklaon <85276514+genmoo@users.noreply.github.com>
2025-05-25 23:53:44 +0900

    동기화 중

--- Assets/Scripts/Manager/MapManager.cs
+++ Assets/Scripts/Manager/MapManager.cs
@@ -1,13 +1,31 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
+using Fusion;
 
-    public class MapManager : MonoBehaviour
+public class MapManager : MonoBehaviour
+{
+    public static MapManager Instance;
+    public Grid grid;
+    public Tilemap wallTilemap;
+    public NetworkRunner runnerPrefab;
+
+    void Awake()
     {
-        public static MapManager Instance;
-        public Grid grid;
+        Instance = this;
 
-        void Awake()
-        {
-            Instance = this;
-        }
     }
+
+//     async void Start()
+//     {
+//         var runner = Instantiate(runnerPrefab);
+//         runner.ProvideInput = true;
+
+//         await runner.StartGame(new StartGameArgs
+//         {
+//             GameMode = GameMode.Shared,
+//             SessionName = "TestRoom",
+//             SceneManager = gameObject.AddComponent<NetworkSceneManagerDefault>()
+//         });
+//     }
+}
 
